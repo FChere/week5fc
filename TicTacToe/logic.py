790b65d9bf3b935d45787d091f53d98a5b4ff956
cwanfeng <cwanfeng@uw.edu>
2022-11-08 15:18:41 -0800

    initial commit

--- TicTacToe/logic.py
+++ TicTacToe/logic.py
@@ -11,12 +11,49 @@ def make_empty_board():
     ]
 
 
-def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    return None  # FIXME
+def get_winner(Board):
+
+    winner = ""
+
+    if Board[0][0] == Board[1][1] == Board[2][2] or Board[0][2] == Board[1][1] == Board[2][0]:
+
+        winner = Board[1][1]
+
+    else:
+
+        for i in range(0,3):
+
+            if Board[i][0] == Board[i][1] == Board[i][2]:
+
+                winner = Board[i][0]
+
+                break
+
+            elif Board[0][i] == Board[1][i] == Board[2][i]:
+
+                winner = Board[0][i]
+
+                break
+
+    if winner == "":
+
+        winner = None
+
+    else:
+
+        print(winner,"Won")
+
+    return winner
+
+
 
 
 def other_player(player):
-    """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+
+    if player == 'X':
+
+        return 'O' 
+
+    elif player == 'O':
+
+        return 'X'
