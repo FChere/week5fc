601599096237449a8135581c7dbf0077cdb13813
cwanfeng <cwanfeng@uw.edu>
2022-11-07 17:59:03 -0800

    Innitial code

--- TicTacToe/cli.py
+++ TicTacToe/cli.py
@@ -2,6 +2,11 @@ from logic import make_empty_board
 
 if __name__ == '__main__':
     board = make_empty_board()
-    winner = winner == None:
-        print("TODO: take a turn!")
-        
+    winner = None
+    while winner == None:
+
+      print("TODO: take a turn!")
+      #
+
+
+      winner = 'x' 
